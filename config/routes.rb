d955800fd777a23c9ebc4a7741ec1507022e0bc2
Ynda Jas <yndajas@gmail.com>
2021-02-27 14:59:24 +0000

    add sessions and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'users#show'
+
+  # sessions
+  get 'login' => 'sessions#new'
+  resources :sessions, only: [:create, :destroy]
+
+  # users
+  get 'register' => 'users#new'
+  get 'dashboard' => 'users#show'
+  get 'account' => 'users#edit'
+  resources :users, only: [:create, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
