f9641a28e614246e0b561f44cb9550f6e9b6e9a9
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-27 18:22:31 +0000

    add OmniAuth login to sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,37 @@
 class SessionsController < ApplicationController
-    # login
     def new
-        @user = User.new
     end
 
     def create
-        # OmniAuth
-        
-        # TODO
-
-        # standard email/password login
-        user = User.find_by(email: params[:email])
-        if user.try(:authenticate, params[:password])
+        if auth
+            # OmniAuth
+            user = User.where(email: auth[:info][:email]).first_or_create do |user|          
+                user.password = SecureRandom.hex
+                user.provider = auth[:provider]
+                user.uid = auth[:uid]
+                user.name = auth[:info][:name]
+            end
             session[:user_id] = user.id
-            redirect_to dashboard_path(user)
+            redirect_to dashboard_path
         else
-            redirect_to login_path, flash: { notice: "Incorrect email and/or password. Please try again." }
+            # standard email/password login
+            user = User.find_by(email: params[:email])
+            if user.try(:authenticate, params[:password])
+                session[:user_id] = user.id
+                redirect_to dashboard_path
+            else
+                redirect_to login_path, flash: { notice: "Incorrect email and/or password. Please try again." }
+            end
         end
     end
 
     def destroy
         session.delete :user_id
+    end
+
+    private
+
+    def auth
+        @auth ||= request.env['omniauth.auth']
     end
 end
