709cf0c3082844c5e9d5d372521db13f98f3eef4
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-27 18:22:02 +0000

    add users controller and show/dashboard action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    # get 'register' => 'users#new'
+    # get 'dashboard' => 'users#show'
+    # get 'account' => 'users#edit'
+    # resources :users, only: [:create, :update, :destroy]
+
+    def show
+    end
+end
