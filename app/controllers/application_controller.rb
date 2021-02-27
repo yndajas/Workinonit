634a7568f4dc42b3d0ecac912ca40f0e840f339d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-27 18:20:50 +0000

    add current user and logged in helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+    def logged_in?
+        !!session[:user_id]
+    end
+    
+    def current_user
+        @user ||= User.find(session[:user_id])
+    end
 end
