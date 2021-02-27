9b5ef4d34446b4c33e5cb61ee5a1c9535e7eb8e6
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-27 15:40:55 +0000

    update sessions#create to reflect non-nested params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,8 @@ class SessionsController < ApplicationController
         # TODO
 
         # standard email/password login
-        user = User.find_by(email: params[:user][:email])
-        if user.try(:authenticate, params[:user][:password])
+        user = User.find_by(email: params[:email])
+        if user.try(:authenticate, params[:password])
             session[:user_id] = user.id
             redirect_to dashboard_path(user)
         else
