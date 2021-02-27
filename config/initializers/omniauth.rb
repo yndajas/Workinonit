0114d617b7bccba204d5117b645b004b8e48e99b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-27 17:15:38 +0000

    add OmniAuth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
+end
