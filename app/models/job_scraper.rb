6800d9f334760a14b48dede47b0eff389dc785ff
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-01 20:34:16 +0000

    remove redundant slug calls
    - code should be passed to scraper methods already slugified

--- app/models/job_scraper.rb
+++ app/models/job_scraper.rb
@@ -1,12 +1,5 @@
 class JobScraper
     def self.scrape_search(keywords, location, country_id = 59)
-        # mass assignment method for slugification - more DRY but more code
-        # bind = binding
-        # {location: location, keywords: keywords}.each {|arg, value| bind.local_variable_set(:"#{arg}", Slugifiable::slugify(value))}
-
-        location = Slugifiable::slugify(location)
-        keywords = Slugifiable::slugify(keywords)
-        
         {Indeed: scrape_indeed_search(keywords, location, country_id), LinkedIn: scrape_linkedin_search(keywords, location), Reed: scrape_reed_search(keywords, location)}
     end
 
