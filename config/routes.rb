53a8e0a852d7e701619c70cff2f29092bd188f64
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-01 20:32:09 +0000

    set route names to helpers

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,13 @@ Rails.application.routes.draw do
   get 'account', to: 'users#edit'
   resources :users, only: [:create, :update, :destroy]
 
+  # search
+  get 'jobs/search/:country_code/:location/:keywords', to: 'search#show', as: 'jobs_search', constraints: {country_code: /[a-z]+/, location: /[a-z0-9\-]+/, keywords: /[a-z0-9\-]+/} # constraints so it only matches with country codes of lower case letters, and locations and keywords of lower case letters, numbers and hyphens
+  post 'jobs/search', to: 'search#create', as: 'jobs_searches'
+  
   # jobs
   resources :jobs
 
-  # search
-  post 'jobs/search', to: 'search#create'
-  get 'jobs/search/:country_code/:location/:keywords', to: 'search#show', constraints: {country_code: /[a-z]+/, location: /[a-z0-9\-]+/, keywords: /[a-z0-9\-]+/} # constraints so it only matches with country codes of lower case letters, and locations and keywords of lower case letters, numbers and hyphens
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
