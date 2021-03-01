f11ded064d7cacfde9a8a7cd62abd6e172b2da56
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-01 19:16:44 +0000

    add constraints to route for search#show
    - also change ` =>` to `, to:`

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@ Rails.application.routes.draw do
   root 'users#show'
 
   # sessions
-  get 'login' => 'sessions#new'
+  get 'login', to: 'sessions#new'
   resources :sessions, only: [:create]
-  delete 'session' => 'sessions#destroy'
+  delete 'session', to: 'sessions#destroy'
 
   # OmniAuth
   get 'auth/facebook/callback', to: 'sessions#create'
 
   # users
-  get 'register' => 'users#new'
-  get 'dashboard' => 'users#show'
-  get 'account' => 'users#edit'
+  get 'register', to: 'users#new'
+  get 'dashboard', to: 'users#show'
+  get 'account', to: 'users#edit'
   resources :users, only: [:create, :update, :destroy]
 
   # jobs
   resources :jobs
 
-  # search - add constraints? -> constraints: { id: /[A-Z][A-Z][0-9]+/ }
-  post 'jobs/search' => 'search#create'
-  get 'jobs/search/:country_code/:location/:keywords' => 'search#show'
+  # search
+  post 'jobs/search', to: 'search#create'
+  get 'jobs/search/:country_code/:location/:keywords', to: 'search#show', constraints: {country_code: /[a-z]+/, location: /[a-z0-9\-]+/, keywords: /[a-z0-9\-]+/} # constraints so it only matches with country codes of lower case letters, and locations and keywords of lower case letters, numbers and hyphens
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
