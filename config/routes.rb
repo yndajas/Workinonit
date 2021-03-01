f5d82217887be6ba52d27cc0986c82d2a7800b65
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-01 18:54:39 +0000

    add job and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,12 @@ Rails.application.routes.draw do
   get 'account' => 'users#edit'
   resources :users, only: [:create, :update, :destroy]
 
+  # jobs
+  resources :jobs
+
+  # search - add constraints? -> constraints: { id: /[A-Z][A-Z][0-9]+/ }
+  post 'jobs/search' => 'search#create'
+  get 'jobs/search/:country_code/:location/:keywords' => 'search#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
