2ed180be54ea1ccb21e9b8fde7a381dd212dc122
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-01 16:32:45 +0000

    add Reed scraper method

--- app/models/job_scraper.rb
+++ app/models/job_scraper.rb
@@ -9,4 +9,23 @@ class JobScraper
         
         {Indeed: scrape_indeed_search(location, keywords), LinkedIn: scrape_linkedin_search(location, keywords), Reed: scrape_reed_search(location, keywords)}
     end
+
+    def self.scrape_reed_search(location, keywords)
+        # open search page
+        search = Nokogiri::HTML(OpenURI.open_uri("https://www.reed.co.uk/jobs/" + keywords + "-jobs-in-" + location))
+
+        # select jobs; limit to first 5
+        first_five_jobs = search.css("article.job-result")[0..4]
+
+        # iterate over jobs, collecting each job's attributes in a hash, then return an array of the hashes
+        first_five_jobs.collect do |job|
+            title_slug_id = job.css("h3.title a")
+            title = title_slug_id.attribute("title").value
+            id = title_slug_id.attribute("data-id").value
+            slug = title_slug_id.attribute("href").value.gsub("/jobs/", "").gsub(/\/#{id}.*/, "")
+            company = job.css("a.gtmJobListingPostedBy").text
+
+            {title: title, company: company, slug: slug, id: id}
+        end
+    end
 end
