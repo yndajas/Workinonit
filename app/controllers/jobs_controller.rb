bdf748f0009573c233ba2ebe391f8d76ea3b9b06
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 20:59:29 +0000

    update method names and arguments
    - use methods that create job and associate with user (pass in current user)
    - also add login requirement

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
     def new
         @countries = ProviderCountry.all
     end
@@ -6,23 +8,23 @@ class JobsController < ApplicationController
     def create
         url = params[:job_url]
 
-        raise params.inspect
-
         # if here from user providing URL on /jobs/new
         if url
             attributes = JobScraper.scrape_job_by_url(url)
 
-            # if attributes variable is a string, process as an error message, else use to create job and redirect to show
+            # if attributes variable is a string, process as an error message, else use to create job (and associate with current user) and redirect to show
             if attributes.is_a?(String)
                 redirect_to new_job_path, flash: { notice: attributes }
             else
-                job = Job.create_from_scraped_attributes(attributes)
+                job = Job.find_or_create_by_scraped_attributes_with_user(attributes, current_user)
                 redirect_to job_path(job)
             end
         # if here from search results (save jobs)
         else
             attributes_hashes = JobScraper.scrape_jobs_by_id_hash(params[:job_ids])
-            attributes_hashes.each {|attributes| Job.create_from_scraped_attributes(attributes)}
+            attributes_hashes.each do |attributes|
+                Job.find_or_create_by_scraped_attributes_with_user(attributes, current_user)
+            end
             redirect_to jobs_path
         end
     end
