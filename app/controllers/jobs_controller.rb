5101d0772536881e1cfb6d92fc8ba04ec313ba42
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 18:57:19 +0000

    update action to deal creating from IDs hash
    - when action is reached via search results page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,17 +5,25 @@ class JobsController < ApplicationController
 
     def create
         url = params[:job_url]
-        if url
-            job = JobScraper::scrape_job_by_url(url)
 
-            # if job is a string, process as an error message, else it should be a Job instance so redirect to the show page
-            if job.is_a?(String)
-                redirect_to new_job_path, flash: { notice: job }
+        raise params.inspect
+
+        # if here from user providing URL on /jobs/new
+        if url
+            attributes = JobScraper.scrape_job_by_url(url)
+
+            # if attributes variable is a string, process as an error message, else use to create job and redirect to show
+            if attributes.is_a?(String)
+                redirect_to new_job_path, flash: { notice: attributes }
             else
+                job = Job.create_from_scraped_attributes(attributes)
                 redirect_to job_path(job)
             end
+        # if here from search results (save jobs)
         else
-            raise params.inspect
+            attributes_hashes = JobScraper.scrape_jobs_by_id_hash(params[:job_ids])
+            attributes_hashes.each {|attributes| Job.create_from_scraped_attributes(attributes)}
+            redirect_to jobs_path
         end
     end
 end
