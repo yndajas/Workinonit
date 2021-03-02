a1f57eb7f5a3b767a4e3a6443191e421577843c2
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 13:40:36 +0000

    update jobs#create error handling
    - JobScraper::scrape_job_by_url will now return either a Job instance or an error message, rather than false - updated controller to process error message

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,10 +7,12 @@ class JobsController < ApplicationController
         url = params[:job_url]
         if url
             job = JobScraper::scrape_job_by_url(url)
-            if job
-                redirect_to job_path(job)
+
+            # if job is a string, process as an error message, else it should be a Job instance so redirect to the show page
+            if job.is_a?(String)
+                redirect_to new_job_path, flash: { notice: job }
             else
-                redirect_to new_job_path, flash: { notice: "URL not recognised. Only indeed.com, linkedin.com and reed.co.uk URLs are supported" }
+                redirect_to job_path(job)
             end
         else
             raise params.inspect
