7b5ae164519e3542600c8cb2d9b7008c0c65c236
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 18:55:59 +0000

    add Job.create_from_scraped_attributes

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,13 @@ class Job < ApplicationRecord
     belongs_to :provider
     has_many :user_jobs
     has_many :users, through: :user_jobs
+
+    def self.create_from_scraped_attributes(attributes)
+        # find or create the company
+        company = Company.find_or_create_by(name: attributes[:company])
+
+        # remove company from the attributes hash then create an associated job using the remaining attributes hash
+        attributes.delete(:company)
+        company.jobs.find_or_create_by(attributes) # might need to be where().first_or_create_by?    
+    end
 end
