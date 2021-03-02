2796bc597cefde6c47453e4845620133636b2e98
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 20:11:35 +0000

    update hash key name

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,10 +9,10 @@ class Job < ApplicationRecord
 
     def self.create_from_scraped_attributes(attributes)
         # find or create the company
-        company = Company.find_or_create_by(name: attributes[:company])
+        company = Company.find_or_create_by(name: attributes[:company_name])
 
         # remove company from the attributes hash then create an associated job using the remaining attributes hash
-        attributes.delete(:company)
+        attributes.delete(:company_name)
         company.jobs.find_or_create_by(attributes) # might need to be where().first_or_create_by?    
     end
 end
