2543ed98692f0a3294712736ca7bbb1667cea314
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-02 20:57:44 +0000

    add method to create job by user and attributes hash
    - also rename methods for more transparency and consistency

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,12 +7,18 @@ class Job < ApplicationRecord
     has_many :user_jobs
     has_many :users, through: :user_jobs
 
-    def self.create_from_scraped_attributes(attributes)
+    def self.find_or_create_by_scraped_attributes(attributes)
         # find or create the company
         company = Company.find_or_create_by(name: attributes[:company_name])
 
         # remove company from the attributes hash then create an associated job using the remaining attributes hash
         attributes.delete(:company_name)
-        company.jobs.find_or_create_by(attributes) # might need to be where().first_or_create_by?    
+        company.jobs.find_or_create_by(attributes)
+    end
+
+    def self.find_or_create_by_scraped_attributes_with_user(attributes, user)
+        job = Job.find_or_create_by_scraped_attributes(attributes)
+        UserJob.find_or_create_by(user_id: user.id, job_id: job.id)
+        job
     end
 end
