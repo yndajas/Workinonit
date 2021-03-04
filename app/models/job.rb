a3540f21c2176e78ea78109f282de2e44825d87d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-04 02:01:10 +0000

    update Job model
    - make provider optional to allow for creation of jobs from data manually entered by users
    - add company_name/company_name= methods
    - add url method that returns either a custom URL (from manual entry creation) or a provider job URL (from scraped data)
    - update method names (more generic) to reflect usage for both scraped and manually entered data

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,22 +3,22 @@ class Job < ApplicationRecord
     include Slugifiable::InstanceMethods
 
     belongs_to :company
-    belongs_to :provider
+    belongs_to :provider, optional: true
     belongs_to :country, optional: true
     has_many :user_jobs
     has_many :users, through: :user_jobs
 
-    def self.find_or_create_by_scraped_attributes(attributes)
+    def self.find_or_create_by_attributes_hash(attributes_hash)
         # find or create the company
-        company = Company.find_or_create_by(name: attributes[:company_name])
+        company = Company.find_or_create_by(name: attributes_hash[:company_name])
 
         # remove company from the attributes hash then create an associated job using the remaining attributes hash
-        attributes.delete(:company_name)
-        company.jobs.find_or_create_by(attributes)
+        attributes_hash.delete(:company_name)
+        company.jobs.find_or_create_by(attributes_hash)
     end
 
-    def self.find_or_create_by_scraped_attributes_with_user(attributes, user)
-        job = Job.find_or_create_by_scraped_attributes(attributes)
+    def self.find_or_create_by_attributes_hash_with_user(attributes, user)
+        job = Job.find_or_create_by_attributes_hash(attributes)
         UserJob.find_or_create_by(user_id: user.id, job_id: job.id)
         job
     end
@@ -28,5 +28,17 @@ class Job < ApplicationRecord
         base_show_url = self.provider.base_show_url_by_country(country_id)
         slug = self.provider_job_slug.try("+", "/") || ""
         base_show_url + slug + self.provider_job_id
+    end
+
+    def company_name=(name)
+        self.company = Company.find_or_create_by(name: name)
+    end
+
+    def company_name
+        self.company.try(:name)
+    end
+
+    def url
+        self.custom_url || self.provider_job_url
     end
 end
