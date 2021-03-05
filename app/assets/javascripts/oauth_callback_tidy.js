9533a6470a9e6c0f393ccc0e5dc489167de09ac3
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-05 18:56:35 +0000

    add code to tidy URL after Google login

--- app/assets/javascripts/oauth_callback_tidy.js
+++ app/assets/javascripts/oauth_callback_tidy.js
@@ -1,4 +1,4 @@
-// credit: https://stackoverflow.com/questions/7131909/facebook-callback-appends-to-return-url/7297873#7297873
+// removes '#_=_' from Facebook callback; credit: https://stackoverflow.com/questions/7131909/facebook-callback-appends-to-return-url/7297873#7297873
 if (window.location.hash && window.location.hash == '#_=_') {
     if (window.history && history.pushState) {
         window.history.pushState("", document.title, window.location.pathname);
@@ -13,4 +13,13 @@ if (window.location.hash && window.location.hash == '#_=_') {
         document.body.scrollTop = scroll.top;
         document.body.scrollLeft = scroll.left;
     }
+}
+
+// removes '#' from Google callback (own code)
+
+var last_character_index = window.location.href.length - 1
+var last_character = window.location.href.charAt(last_character_index)
+
+if (last_character == '#') {
+    window.location.href = window.location.href.substring(0, last_character_index)
 }
