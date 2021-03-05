edb2460831047ed298e1f26676545baa3e8ad205
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-05 18:55:54 +0000

    add Google OAuth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
+    provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
 end
