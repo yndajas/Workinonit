20f20d5b886620d717ef3b3d6741ce482edc2a50
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-05 21:49:56 +0000

    configure GitHub OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
+    provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], scope: "user:email"
     provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
 end
