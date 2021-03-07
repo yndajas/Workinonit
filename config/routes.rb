57701a7fe81bdbe1281822b7559d7e97333dc135
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-07 19:03:22 +0000

    update jobs and searches routes; add initial applications routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,15 +19,22 @@ Rails.application.routes.draw do
   get 'account', to: 'users#edit'
   resources :users, only: [:create, :update, :destroy]
 
-  # search
+  # searches
   scope '/jobs' do
-    get 'search/:country_code/:location/:keywords', to: 'search#show', as: 'jobs_search', constraints: {country_code: /[a-z]+/, location: /[a-z0-9\-]+/, keywords: /[a-z0-9\-]+/} # constraints so it only matches with country codes of lower case letters, and locations and keywords of lower case letters, numbers and hyphens
-    post 'search', to: 'search#create', as: 'jobs_searches'
+    get 'search/:country_code/:location/:keywords', to: 'searches#show', as: 'jobs_search', constraints: {country_code: /[a-z]+/, location: /[a-z0-9\-]+/, keywords: /[a-z0-9\-]+/} # constraints so it only matches with country codes of lower case letters, and locations and keywords of lower case letters, numbers and hyphens
+    post 'searches', to: 'searches#create', as: 'jobs_searches'
   end
   
   # jobs
-  resources :jobs
+  resources :jobs, only: [:new, :create, :index]
+  get 'jobs/:id/:slug', to: 'jobs#show', as: 'job'
+  get 'jobs/:id/:slug/edit', to: 'jobs#edit', as: 'edit_job'
+  patch 'jobs/:id/:slug', to: 'jobs#update'
+  delete 'jobs/:id/:slug', to: 'jobs#destroy'
+  get 'jobs/:id/:slug/apply', to: 'applications#new',as: 'new_application_by_job'
 
+  # applications
+  resources :applications
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
