bcc609b37bbbdcaf5b6187ea920388c79b4dcc93
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 11:27:27 +0000

    add UserJob scope

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,6 @@ class Application < ApplicationRecord
     belongs_to :user
     belongs_to :job
     belongs_to :status
+
+    scope :user_job, ->(user_job) {where(user_id: user_job.user_id, job_id: user_job.job_id)}
 end
