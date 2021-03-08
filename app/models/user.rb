16d35008b99106b4b6cdfacda58c5b847cedd60e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 18:52:38 +0000

    create custom User#companies method to return unique records
    - and update companies alphabetical method that was using the previous ActiveRecord-based method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
     has_many :user_jobs
     has_many :jobs, through: :user_jobs
-    has_many :companies, through: :jobs
     has_many :user_company_information
     has_many :applications
     has_secure_password
 
+    def companies
+        self.jobs.collect { |job| job.company }.uniq
+    end
+
     def companies_alphabetical
-        self.companies.order(:name)
+        self.companies.sort_by(&:name)
     end
 end
