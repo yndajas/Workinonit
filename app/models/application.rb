a7fea53fe9152610f8f01311c2860c17b8e71713
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 12:10:41 +0000

    add find_by_user_job method

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,8 @@ class Application < ApplicationRecord
     belongs_to :status
 
     scope :user_job, ->(user_job) {where(user_id: user_job.user_id, job_id: user_job.job_id)}
+
+    def self.find_by_user_job(user_job)
+        self.user_job(user_job)[0]
+    end
 end
