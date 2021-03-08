7a06c2314aee95f50b8bcb36916bc7fb3cf9a0e9
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 12:11:16 +0000

    add/rename find by/association methods

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -5,23 +5,35 @@ class UserJob < ApplicationRecord
     scope :user, ->(user) {where(user_id: user.id)}
     scope :job, ->(job) {where(job_id: job.id)}
 
-    def self.by_user_reverse_chronological(user)
+    def self.find_by_user_reverse_chronological(user)
         self.user(user).order(created_at: :desc)
     end
 
-    def self.by_user_and_company_reverse_chronological(user, company)
+    def self.find_by_user_and_company_reverse_chronological(user, company)
         self.by_user_reverse_chronological(user).collect do |user_job|
             user_job if user_job.company == company
         end.compact
     end
 
-    def self.unapplied_by_user_reverse_chronological(user)
+    def self.find_by_user_and_unapplied_reverse_chronological(user)
         self.by_user_reverse_chronological(user).collect do |user_job|
             user_job if Application.user_job(user_job).length == 0
         end.compact
     end
 
+    def self.find_by_job(job)
+        self.job(job)[0]
+    end
+
+    def self.find_by_user_and_job(user, job)
+        self.user(user).job(job)[0]
+    end
+
     def company
         self.job.company
+    end
+
+    def application
+        Application.find_by_user_job(self)
     end
 end
