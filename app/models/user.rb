afc9868b45dd5977774f19673c1a7066481cd69f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 11:28:08 +0000

    add method for getting user's companies alphabetised

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     has_many :user_company_information
     has_many :applications
     has_secure_password
+
+    def companies_alphabetical
+        self.companies.order(:name)
+    end
 end
