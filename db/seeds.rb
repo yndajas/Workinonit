f69d81e4ef6413dee4b1534acbe574688565865f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 18:38:54 +0000

    add seed data for testing user deletion

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: "test@g.co", password: "test", name: "Testy McTestface")
+
+deletable_user = User.create(email: "delete-me@g.co", password: "test", name: "Deletey")
+company = Company.create(name: "Wurky placeSA")
+deletable_job = Job.create(title: "test job title JOJPKW", company_id: company.id)
+undeletable_job = Job.create(title: "great job security for WEWEKLJ", company_id: company.id, provider_id: 1)
+deletable_user_job_for_deletable_job = UserJob.create(user_id: deletable_user.id, job_id: deletable_job.id)
+deletable_user_job_for_undeletable_job = UserJob.create(user_id: deletable_user.id, job_id: undeletable_job.id)
+deletable_application_for_deletable_job = Application.create(user_id: deletable_user.id, job_id: deletable_job.id, status_id: 1)
+deletable_application_for_undeletable_job = Application.create(user_id: deletable_user.id, job_id: undeletable_job.id, status_id: 1)
+deletable_user_company_information = UserCompanyInformation.create(user_id: deletable_user.id, company_id: company.id)
