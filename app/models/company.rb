b5feec985323aaa2d21ece57e3f77d535add8f64
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 18:54:22 +0000

    remove has_many_through has_many_through association

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ApplicationRecord
     include Slugifiable::InstanceMethods
     
     has_many :jobs
-    has_many :users, through: :jobs
+    has_many :user_jobs, through: :jobs
     has_many :applications, through: :jobs
     has_many :user_company_information
 end
