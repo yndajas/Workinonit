f1a8af1851f6b571fe1b8fe0118945196093bad0
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-08 11:29:29 +0000

    add user scope method; other filter methods; company method

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -1,4 +1,26 @@
 class UserJob < ApplicationRecord
     belongs_to :user
     belongs_to :job
+
+    scope :user, ->(user) {where(user_id: user.id)}
+
+    def self.by_user_reverse_chronological(user)
+        self.user(user).order(created_at: :desc)
+    end
+
+    def self.by_user_and_company_reverse_chronological(user, company)
+        self.by_user_reverse_chronological(user).collect do |user_job|
+            user_job if user_job.company == company
+        end.compact
+    end
+
+    def self.unapplied_by_user_reverse_chronological(user)
+        self.by_user_reverse_chronological(user).collect do |user_job|
+            user_job if Application.user_job(user_job).length == 0
+        end.compact
+    end
+
+    def company
+        self.job.company
+    end
 end
