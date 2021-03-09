ccf0047928714130aba639920e286bbac653dd30
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:32:00 +0000

    add methods to get user's companies with applications

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
     def companies_alphabetical
         self.companies.sort_by(&:name)
     end
+
+    def companies_with_applications
+        self.jobs.collect do |job|
+            user_job = UserJob.find_by_user_and_job(self, job)
+            job.company if Application.find_by_user_job(user_job)
+        end.compact.uniq
+    end
+
+    def companies_with_applications_alphabetical
+        self.companies_with_applications.sort_by(&:name)
+    end
 end
