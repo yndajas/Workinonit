20f91a144a61a59a28e616476d7ef7cd205cf2ab
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:35:48 +0000

    make has_value_for? work for Date objects

--- app/models/concerns/attributable.rb
+++ app/models/concerns/attributable.rb
@@ -1,7 +1,10 @@
 module Attributable
     module InstanceMethods
         def has_value_for?(attribute)
-            !!self.send(attribute).try(:length).try(:>, 0)
+            attribute = self.send(attribute)
+            
+            # if it's a Date or not nil and not empty return true
+            !!(attribute.try(:class).try(:==, Date) || attribute.try(:length).try(:>, 0))
         end
     end
 end
