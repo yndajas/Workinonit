dd1e7f475d130df068aa4f9bcff841e2b7510780
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:40:21 +0000

    add helpers; add index/filter/edit/update actions; update new/show actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -13,19 +13,16 @@ class ApplicationsController < ApplicationController
             redirect_to jobs_path, flash: {type: 'warning', content: "Job not found"} if !@job
 
             # redirect if an application for the job (by the current user) already exists 
-            existing_application = Application.find_by_user_job(UserJob.find_by_user_and_job(current_user, @job))
+            user_job = UserJob.find_by_user_and_job(current_user, @job)
+            existing_application = Application.find_by_user_job(user_job)
             redirect_to application_path(existing_application), flash: {type: 'warning', content: "Application already exists"} if existing_application
-
-            @application.job = @job
         # if here via /applications/new
         else
             @jobs = Job.find_by_user_and_unapplied_alphabetical(current_user)
         end
 
-        @statuses = Status.all
-
-        @progress_booleans = {checked_job_requirements: "Checked job requirements", researched_company: "Researched company", made_contact: "Made informal contact", prepared_cv: "Prepared CV or entered details online", prepared_cover_letter: "Prepared cover letter or personal statement"}
-        @dates = {date_found: "Found job", date_applied: "Submitted application", interview_date: "Interview", date_outcome_received: "Received outcome"}    
+        set_progress_and_dates_instance_variables                
+        set_statuses
     end
 
     def create
@@ -35,11 +32,94 @@ class ApplicationsController < ApplicationController
 
     def show
         @application = Application.find(params[:id])
+        redirect_if_no_application_or_does_not_belong_to_user
+
+        set_progress_and_dates_instance_variables
+    end
+
+    def index
+        # get filtered and ordered company/ies and user jobs
+        # if here from /companies/:id/:slug/applications
+        if params[:id]
+            @company = Company.find_by_id(params[:id])
+            
+            if @company
+                @applications = Application.find_by_user_and_company_reverse_by_date(current_user, @company, :updated_at)
+                redirect_to companies_path, flash: {type: 'warning', content: "No saved applications found at company"} if @applications.length == 0
+            else
+                redirect_to companies_path, flash: {type: 'warning', content: "Company not found"}
+            end
+        # if here from /applications/status/:slug
+        elsif status_filter_request?
+            slug = status_filter_slug
+            @status = Status.find_by_slug(slug)
+            @applications = Application.find_by_user_and_status_reverse_by_date(current_user, @status, :updated_at)
+            redirect_to applications_path, flash: {type: 'warning', content: "No saved applications with status \"#{@status.name.downcase}\""} if @applications.length == 0
+        # if here from /applications
+        else
+            @companies = current_user.companies_with_applications_alphabetical
+            @statuses = Status.all
+            @applications = Application.find_by_user_reverse_by_date(current_user, :updated_at)
+        end
+
+        # if here from /companies/:id/:slug/applications or /applications/status/:slug, render special view (else default to regular index)
+        if @applications.length > 0 && (params[:id] || status_filter_request?)
+            @title = (params[:id] ? "Applications for #{@company.name}" : "Applications: #{@status.name.downcase}")
+            render 'filtered_index'
+        end
+    end
+
+    def filter
+        if params[:company_id]
+            company = Company.find(params[:company_id])
+            redirect_to company_applications_path(company, company.slug)
+        else
+            status = Status.find(params[:status_id])
+            redirect_to applications_by_status_path(status.slug)
+        end
+    end
+
+    def edit
+        @application = Application.find(params[:id])
+        redirect_if_no_application_or_does_not_belong_to_user
+
+        @job = @application.job
+        set_progress_and_dates_instance_variables
+        set_statuses
+    end
+
+    def update
+        application = Application.find_by_id(params[:id])
+        application.update(application_params)
+        redirect_to application_path(application), flash: {type: 'success', content: "Application successfully updated"}
     end
 
     private
 
     def application_params
         params.require(:application).permit(:job_id, :status_id, :checked_job_requirements, :researched_company, :made_contact, :prepared_cv, :prepared_cover_letter, :date_found, :date_applied, :interview_date, :date_outcome_received, :notes, :feedback)
+    end
+
+    def status_filter_request?
+        !!(request.path.downcase =~ /\/applications\/status\/.+/)
+    end
+
+    def status_filter_slug
+        request.path.downcase.gsub("/applications/status/", "")
+    end
+
+    def set_progress_and_dates_instance_variables
+        @progress_booleans = {checked_job_requirements: "Checked job requirements", researched_company: "Researched company", made_contact: "Made informal contact", prepared_cv: "Prepared CV or entered details online", prepared_cover_letter: "Prepared cover letter or personal statement"}
+        @dates = {date_found: "Found job", date_applied: "Submitted application", interview_date: "Interview", date_outcome_received: "Received outcome"}
+    end
+
+    def set_statuses
+        @statuses = Status.all
+    end
+
+    def redirect_if_no_application_or_does_not_belong_to_user
+        unless @application.try(:user).try(:==, current_user)
+            redirect_to applications_path, flash: {type: 'warning', content: "Application not found"}
+        end
     end
 end
