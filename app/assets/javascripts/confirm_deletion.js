e828dce6b7dc0ebf02e1d31a87cbf6d9e162c5ea
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 13:36:17 +0000

    add deletion confirmation message for applications

--- app/assets/javascripts/confirm_deletion.js
+++ app/assets/javascripts/confirm_deletion.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
         var model = e.target.className.replace("edit_", "").replace(/ .*/, "")
 
         switch(model) {
-            case "job":
-                var message = "deleting a job cannot be undone. Deleting a job will also delete any associated application data.\n\nAre you sure you wish to delete this job?";
-                break;
             case "user":
                 var message = "deleting your account cannot be undone. You will lose all data, including saved jobs and applications.\n\nAre you sure you wish to delete your account and all associated data?";
                 break;
-        }
+            case "job":
+                var message = "deleting a job cannot be undone. Deleting a job will also delete any associated application data.\n\nAre you sure you wish to delete this job?";
+                break;
+            case "application":
+                var message = "deleting an application cannot be undone.\n\nAre you sure you wish to delete this application?";
+                break;
+            }
 
         return confirm(`Warning: ${message}`);
         e.preventDefault();
