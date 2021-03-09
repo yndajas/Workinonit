fe12c62e9b1c21d732ab268824acfa0b4e8c029d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:33:59 +0000

    use tag helper for br in module

--- app/models/concerns/format_and_sanitizable.rb
+++ app/models/concerns/format_and_sanitizable.rb
@@ -1,7 +1,7 @@
 module FormatAndSanitizable
     module InstanceMethods
         def format_and_sanitize(attribute)
-            ActionController::Base.helpers.sanitize(self.send(attribute).gsub(/\r?\n|\r/, "<br>"))
+            ActionController::Base.helpers.sanitize(self.send(attribute).gsub(/\r?\n|\r/, ActionController::Base.helpers.tag.br))
         end
 
         def sanitize(attribute)
