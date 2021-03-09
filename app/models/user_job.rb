c15d928fffc71d5904ccd377538a33d7ffca824b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:31:37 +0000

    abstract find methods to module where reusable

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -1,22 +1,14 @@
 class UserJob < ApplicationRecord
+    extend Findable::ClassMethods
+
     belongs_to :user
     belongs_to :job
 
     scope :user, ->(user) {where(user_id: user.id)}
     scope :job, ->(job) {where(job_id: job.id)}
 
-    def self.find_by_user_reverse_chronological(user)
-        self.user(user).order(created_at: :desc)
-    end
-
-    def self.find_by_user_and_company_reverse_chronological(user, company)
-        self.find_by_user_reverse_chronological(user).collect do |user_job|
-            user_job if user_job.company == company
-        end.compact
-    end
-
-    def self.find_by_user_and_unapplied_reverse_chronological(user)
-        self.find_by_user_reverse_chronological(user).collect do |user_job|
+    def self.find_by_user_and_unapplied_reverse_by_date(user, date)
+        self.find_by_user_reverse_by_date(user, date).collect do |user_job|
             user_job if !Application.find_by_user_job(user_job)
         end.compact
     end
