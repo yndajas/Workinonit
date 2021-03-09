a2adf6de4c736322a6de70bed8fdbf99cc6c2a94
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-09 23:34:51 +0000

    fix class method notation; add find by user and sort by date methods

--- app/models/concerns/findable.rb
+++ app/models/concerns/findable.rb
@@ -3,9 +3,19 @@ module Findable
         def self.extended(base)
             base.scope :id, ->(id) {where(id: id)}
         end
-            
-        def self.find_by_id(id)
+
+        def find_by_id(id)
             self.id(id)[0]
         end
+
+        def find_by_user_reverse_by_date(user, date_field)
+            self.user(user).order(:"#{date_field}" => :desc)
+        end
+    
+        def find_by_user_and_company_reverse_by_date(user, company, date_field)
+            self.find_by_user_reverse_by_date(user, date_field).collect do |object|
+                object if object.company == company
+            end.compact
+        end    
     end
 end
