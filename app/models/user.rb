8c08e09b1b24845df52ec0ed466038ceeabd393e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-10 16:46:24 +0000

    refactor User#companies_with_applications method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,7 @@ class User < ApplicationRecord
     end
 
     def companies_with_applications
-        self.jobs.collect do |job|
-            user_job = UserJob.find_by_user_and_job(self, job)
-            job.company if Application.find_by_user_job(user_job)
-        end.compact.uniq
+        Application.user(self).collect { |application| application.company }.uniq
     end
 
     def companies_with_applications_alphabetical
