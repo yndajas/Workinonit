9ecfc66ac896258d3039df084380403a0b87e64b
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-11 03:44:06 +0000

    add deletion confirmation message for feedback

--- app/assets/javascripts/confirm_deletion.js
+++ app/assets/javascripts/confirm_deletion.js
@@ -10,8 +10,14 @@ $(document).ready(function(){
                 var message = "deleting a job cannot be undone. Deleting a job will also delete any associated application data.\n\nAre you sure you wish to delete this job?";
                 break;
             case "application":
-                var message = "deleting an application cannot be undone.\n\nAre you sure you wish to delete this application?";
-                break;
+                // check if action ends 'feedback'
+                if (e.target.action.substring(e.target.action.length - 8, e.target.action.length) == "feedback") {
+                    var message = "deleting feedback cannot be undone.\n\nAre you sure you wish to delete this feedback?";
+                    break;    
+                } else {
+                    var message = "deleting an application cannot be undone.\n\nAre you sure you wish to delete this application?";
+                    break;    
+                }
             }
 
         return confirm(`Warning: ${message}`);
