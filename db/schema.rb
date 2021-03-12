1431697455aa9a9da2c71cdeb84adda72a716747
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-12 20:19:47 +0000

    update schema to work with PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_04_191649) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "applications", force: :cascade do |t|
     t.integer "user_id"
     t.integer "job_id"
