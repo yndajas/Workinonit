0dcf1677aa03a7dcc8428529aac92bfe8f334619
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-12 22:11:58 +0000

    add validations to user company information

--- app/models/user_company_information.rb
+++ app/models/user_company_information.rb
@@ -4,4 +4,7 @@ class UserCompanyInformation < ApplicationRecord
 
     belongs_to :user
     belongs_to :company
+
+    validates :website, presence: true, length: {minimum: 1}, unless: :notes?
+    validates :notes, presence: true, length: {minimum: 1}, unless: :website?
 end
