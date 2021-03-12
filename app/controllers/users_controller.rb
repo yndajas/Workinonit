afd6cd83094a356d486bbb20d710546fc5b94e64
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-12 15:41:33 +0000

    flesh out show (dashboard) and update (account) actions
    - also make separate strong params methods for create and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,45 @@ class UsersController < ApplicationController
     end
 
     def create
-        if User.find_by(email: user_params[:email])
-            redirect_to login_path, flash: {type: 'warning', content: "User with email #{user_params[:email]} already exists. Try logging in"}
+        if User.find_by(email: user_create_params[:email])
+            redirect_to login_path, flash: {type: 'warning', content: "User with email #{user_create_params[:email]} already exists. Try logging in"}
         else
-            user = User.create(user_params)
+            user = User.create(user_create_params)
             session[:user_id] = user.id
             redirect_to dashboard_path, flash: {type: 'success', content: "Account created. Time to get Workinonit!"}
         end        
     end
 
     def show
+        @job_count = UserJob.user(current_user).length
+        @open_applications_count = Application.find_by_user_and_open_reverse_by_date(current_user, :updated_at).length
+        @jobs_without_applications_count = Job.find_by_user_and_unapplied_alphabetical(current_user).length
+        @feedback_count = Application.find_by_user_reverse_by_date_with_feedback(current_user, :updated_at).length
+        @applications_awaiting_feedback_count = Application.find_unsuccessful_by_user_without_feedback_alphabetical(current_user).length
     end
 
     def edit
+    end
+
+    def update
+        if user_update_params[:name]
+            current_user.update(name: user_update_params[:name])
+            redirect_to account_path, flash: {type: 'success', content: "Name successfully updated"}
+        elsif !user_update_params[:current_password] || current_user.authenticate(user_update_params[:current_password])
+            if user_update_params[:new_email]
+                if User.find_by(email: user_update_params[:new_email])
+                    redirect_to account_path, flash: {type: 'warning', content: "Account with email #{user_update_params[:new_email]} already exists"}
+                else
+                    current_user.update(email: user_update_params[:new_email])
+                    redirect_to account_path, flash: {type: 'success', content: "Email successfully updated"}
+                end
+            else
+                current_user.update(password: user_update_params[:new_password], random_password: false)
+                redirect_to account_path, flash: {type: 'success', content: "Password successfully updated"}
+            end
+        else
+            redirect_to account_path, flash: {type: 'danger', content: "Incorrect password"}
+        end
     end
 
     def destroy
@@ -36,7 +62,11 @@ class UsersController < ApplicationController
 
     private
 
-    def user_params
+    def user_create_params
         params.permit(:name, :email, :password)
+    end
+
+    def user_update_params
+        params.require(:user).permit(:name, :email, :new_email, :new_password, :current_password)
     end
 end
